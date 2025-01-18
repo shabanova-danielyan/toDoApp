71aae52c3298ede8bc89b2a71c46ca791d5345c0
Elizaveta <elizaveta.shabanova-danielyan@learnit.fi>
2025-01-18 19:42:31 +0200

    Improved function readUserInputAndDisplayItOld to add ids with dynamic numbers

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let toDoTasks = [];
+//let toDoTasks = [];
 
-function addTask(newTask) {
-    toDoTasks.push(newTask);
-}
+//function addTask(newTask) {
+//    toDoTasks.push(newTask);
+//}
 
 //Old Adds a new task as a paragraph. Old code is commented on HTML page.
 //function readUserInputAndDisplayIt() {
@@ -19,18 +19,44 @@ function addTask(newTask) {
 //}
 
 //New Adds a new task + buttons ('Clear list', 'Mark as done') as lines in a table. HTML page in adjusted
-function readUserInputAndDisplayIt() {
+
+function readUserInputAndDisplayItNew() {
     const userInput = document.querySelector('.textInput').value;
-    const html = `<tr id=individual-task>
+    const listOfTasks = document.querySelectorAll('.individual-task');
+
+    if (listOfTasks) {
+        const htmlTableRow = `<tr class=individual-task id=${listOfTasks.length}>
+                    <th>${userInput}</th>
+                    <th><button class='remove-individual-task-btn''>Remove task</button></th>
+                    <th><button class='mark-as-done-btn'>Mark as done</button></th>
+                  </tr>`;
+        const tasksContainer = document.getElementById("tasks-container");
+        tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
+    } else { 
+        const htmlTableRow = `<tr class=individual-task id=${document.querySelectorAll('.individual-task').length}>
                     <th>${userInput}</th>
                     <th><button class='remove-individual-task-btn''>Remove task</button></th>
                     <th><button class='mark-as-done-btn'>Mark as done</button></th>
                   </tr>`;
     const tasksContainer = document.getElementById("tasks-container");
-    tasksContainer.insertAdjacentHTML('beforeend', html)
-    addTask(userInput);
-    console.log(toDoTasks);
+    tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
+    }
+
+    
 }
+
+//function readUserInputAndDisplayItOld() {
+//    const userInput = document.querySelector('.textInput').value;
+//    const htmlTableRow = `<tr id=individual-task>
+//                    <th>${userInput}</th>
+//                    <th><button class='remove-individual-task-btn''>Remove task</button></th>
+//                    <th><button class='mark-as-done-btn'>Mark as done</button></th>
+//                  </tr>`;
+//    const tasksContainer = document.getElementById("tasks-container");
+//    tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
+//    addTask(userInput);
+//    console.log(toDoTasks);
+//}
 
 //Relevant for the case when tasks are paragraphs inside a <div>
 //function removeTasksFromPage() {
@@ -43,23 +69,28 @@ function readUserInputAndDisplayIt() {
 //New version when tasks are rows in a table
 function removeTasksFromPage() {
    const tableWithTasksList = document.getElementById('tasks-container');
-   const parent = tableWithTasksList.parentNode;
-   parent.removeChild(tableWithTasksList);
-   console.log(tableWithTasksList);
+   tableWithTasksList.innerHTML = "";
+   console.log("Table is cleared", tableWithTasksList);
 }
 
-function clearList(toDoTasks) {
-    toDoTasks = [];
-    console.log(toDoTasks);
-    removeTasksFromPage(toDoTasks);
+//function clearList() {
+//    toDoTasks = [];
+//    console.log(toDoTasks);
+//    removeTasksFromPage();
+//}
+
+function removeOneTaskFromPage () {
+
 }
 
-//IN PROGRESS function removeOneTask () {}
+function removeOneTask () {
+
+}
 
 //IN PROGRESS function markAsDone () {}
 
-document.querySelector('#submit-btn').addEventListener('click', readUserInputAndDisplayIt);
-document.querySelector('#clear-btn').addEventListener('click', clearList);
+document.querySelector('#submit-btn').addEventListener('click', readUserInputAndDisplayItNew);
+document.querySelector('#clear-btn').addEventListener('click', removeTasksFromPage);
 //IN PROGRESS document.querySelector('#').addEventListener('', removeOneTask);
 //IN PROGRESS document.querySelector().addEventListener('',markAsDone);
 
