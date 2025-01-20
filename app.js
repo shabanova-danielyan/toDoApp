028e0715464cbfaf5035ebc1f93038d8fa78a9ea
Elizaveta <elizaveta.shabanova-danielyan@learnit.fi>
2025-01-20 13:38:49 +0200

    Finnished a function removing one selected task from page

--- app.js
+++ app.js
@@ -1,70 +1,28 @@
 'use strict';
 
-//let toDoTasks = [];
-
-//function addTask(newTask) {
-//    toDoTasks.push(newTask);
-//}
-
-//Old Adds a new task as a paragraph. Old code is commented on HTML page.
-//function readUserInputAndDisplayIt() {
-//    const userInput = document.querySelector('.textInput').value;
-//    const html = `<p id=individual-task>${userInput}</p>
-//                  <button class='remove-individual-task-btn''>Remove task</button>
-//                  <button class='mark-as-done-btn'>Mark as done</button>`;
-//    const tasksContainer = document.getElementById("tasks-container");
-//    tasksContainer.insertAdjacentHTML('beforeend', html)
-//    addTask(userInput);
-//    console.log(toDoTasks);
-//}
-
-//New Adds a new task + buttons ('Clear list', 'Mark as done') as lines in a table. HTML page in adjusted
-
 function readUserInputAndDisplayItNew() {
     const userInput = document.querySelector('.textInput').value;
     const listOfTasks = document.querySelectorAll('.individual-task');
+    let nItems = listOfTasks.length; //TODO: make it in a normal way
+    let newID =  nItems;
+    
+    const htmlTableRow = `<tr class=individual-task id=tableRow${newID}>
+                <th>${userInput}</th>
+                <th><button class='remove-individual-task-btn' id=btnRemoveItem${newID}>Remove task</button></th>
+                <th><button class='mark-as-done-btn' id=btnMarkDone${newID}>Mark as done</button></th>
+              </tr>`;
 
-    if (listOfTasks) {
-        const htmlTableRow = `<tr class=individual-task id=${listOfTasks.length}>
-                    <th>${userInput}</th>
-                    <th><button class='remove-individual-task-btn''>Remove task</button></th>
-                    <th><button class='mark-as-done-btn'>Mark as done</button></th>
-                  </tr>`;
-        const tasksContainer = document.getElementById("tasks-container");
-        tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
-    } else { 
-        const htmlTableRow = `<tr class=individual-task id=${document.querySelectorAll('.individual-task').length}>
-                    <th>${userInput}</th>
-                    <th><button class='remove-individual-task-btn''>Remove task</button></th>
-                    <th><button class='mark-as-done-btn'>Mark as done</button></th>
-                  </tr>`;
     const tasksContainer = document.getElementById("tasks-container");
     tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
-    }
+    console.log(htmlTableRow);
+    document.querySelector(`#btnRemoveItem${newID}`).addEventListener('click', ()=>removeOneTaskFromPage(newID));
 
-    
 }
 
-//function readUserInputAndDisplayItOld() {
-//    const userInput = document.querySelector('.textInput').value;
-//    const htmlTableRow = `<tr id=individual-task>
-//                    <th>${userInput}</th>
-//                    <th><button class='remove-individual-task-btn''>Remove task</button></th>
-//                    <th><button class='mark-as-done-btn'>Mark as done</button></th>
-//                  </tr>`;
-//    const tasksContainer = document.getElementById("tasks-container");
-//    tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
-//    addTask(userInput);
-//    console.log(toDoTasks);
-//}
-
-//Relevant for the case when tasks are paragraphs inside a <div>
-//function removeTasksFromPage() {
-//   const divWithTasksList = document.getElementById('tasks-container');
-//   console.log(divWithTasksList, typeof divWithTasksList);
-//   const parent = divWithTasksList.parentNode;
-//   parent.removeChild(divWithTasksList);
-//}
+function removeOneTaskFromPage (id) {
+    console.log(`Task with id ${id} has been removed.`);
+    document.getElementById(`tableRow${id}`).remove();
+}
 
 //New version when tasks are rows in a table
 function removeTasksFromPage() {
@@ -73,24 +31,9 @@ function removeTasksFromPage() {
    console.log("Table is cleared", tableWithTasksList);
 }
 
-//function clearList() {
-//    toDoTasks = [];
-//    console.log(toDoTasks);
-//    removeTasksFromPage();
-//}
-
-function removeOneTaskFromPage () {
-
-}
-
-function removeOneTask () {
-
-}
-
 //IN PROGRESS function markAsDone () {}
 
 document.querySelector('#submit-btn').addEventListener('click', readUserInputAndDisplayItNew);
 document.querySelector('#clear-btn').addEventListener('click', removeTasksFromPage);
-//IN PROGRESS document.querySelector('#').addEventListener('', removeOneTask);
-//IN PROGRESS document.querySelector().addEventListener('',markAsDone);
+//IN PROGRESS document.querySelector(.mark-as-done-btn).addEventListener('click',markAsDone);
 
