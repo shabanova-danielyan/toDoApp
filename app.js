7e94bc31d3f398e0c80edfd83120cee2371a21f5
Elizaveta <elizaveta.shabanova-danielyan@learnit.fi>
2025-01-22 12:24:04 +0200

    Code improvements

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 'use strict';
 
-const tasks = {};
+let tasksObject = {};
 
-//function findLargestIdInList() {
-//    const listOfTasksIds = tasks.key;
-//    const largestIdInList = () => 
-//}
+function findLargestIdInList() {
+    const listOfTasksIds = Object.keys(tasksObject);
+    const numberOfTasksInToDoList = listOfTasksIds.length;
+    let largestIdInList = 0;
+    for (let i=0; i<numberOfTasksInToDoList+1; i++) {
+        if (largestIdInList<listOfTasksIds[i]) {
+            largestIdInList = listOfTasksIds[i];
+        };
+    };
+}
 
 function readUserInputAndDisplayItNew() {
     const userInput = document.querySelector('.textInput').value;
-    const numberOfTasksInToDoList = tasks.key.length;
-
+    const listOfTasksIds = Object.keys(tasksObject);
+    const numberOfTasksInToDoList = listOfTasksIds.length;
+    
+    let newID;
     if (numberOfTasksInToDoList == 0) {
-        const htmlTableRow = `<tr class="individual-task" id="tableRow1">
-        <th class="rowText" id="taskName1">${userInput}</th>
-        <th><button class="button" id="btnRemoveItem1">Remove task</button></th>
-        <th><button class="button" id="btnMarkDone1">Mark as done</button></th>
-        </tr>`;
-        const tasksContainer = document.getElementById("tasks-container");
-        tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
-        console.log(htmlTableRow);
-        const tasks = {
-            1: {
-                taskDescription: `${userInput}`,
-                status: 'Not done'
-                }
-            };
-    } else {
-        const newID = findLargestIdInList() ++;
-        const htmlTableRow = `<tr class=individual-task id=tableRow${newID}>
-        <th class="rowText" id=taskName${newID}>${userInput}</th>
-        <th><button class="button" id=btnRemoveItem${newID}>Remove task</button></th>
-        <th><button class="button" id=btnMarkDone${newID}>Mark as done</button></th>
-        </tr>`;
-        const tasksContainer = document.getElementById("tasks-container");
-        tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
-        console.log(htmlTableRow);
-        tasks.newID = {
-            taskDescription: `${userInput}`,
-            status: 'Not done'
+        newID = 1;
+        } else {
+        newID = findLargestIdInList() + 1;
         };
-    }
+    
+    const tasksContainer = document.getElementById("tasks-container");
 
+    const htmlTableRow = `<tr class="individual-task" id="tableRow${newID}">
+    <th class="rowText" id="taskName${newID}">${userInput}</th>
+    <th><button class="button" id="btnRemoveItem${newID}">Remove task</button></th>
+    <th><button class="button" id="btnMarkDone${newID}">Mark as done</button></th>
+    </tr>`;
+    tasksContainer.insertAdjacentHTML('beforeend', htmlTableRow);
+    console.log(htmlTableRow);
+
+    tasksObject['id${newID}'] = {
+        taskDescription: `${userInput}`,
+        status: 'Not done'
+    };
+    
     document.querySelector(`#btnRemoveItem${newID}`).addEventListener('click', ()=>removeOneTaskFromPage(newID));
     document.querySelector(`#btnMarkDone${newID}`).addEventListener('click', ()=>onClick(newID));
 }
 
 function removeOneTaskFromPage (id) {
     document.getElementById(`tableRow${id}`).remove();
-    delete tasks.id;
+    delete tasksObject['id${newID}'];
     console.log(`Task with id ${id} has been removed.`);
 }
 
@@ -56,24 +54,24 @@ function onClick (id) {
     const buttonMarkAsDone = document.getElementById(`btnMarkDone${id}`);
     const taskHTML = document.getElementById(`taskName${id}`);
 
-    const task = tasks.id;
-    if (task.status == "Not done")
+    const singleTask = tasksObject['id${id}'];
+    if (singleTask.status == "Not done")
     {
         taskHTML.style.textDecoration = "line-through";
         buttonMarkAsDone.textContent = "Undo";
-        task.status = "Done";
+        singleTask.status = "Done";
     }
     else{
         taskHTML.style.textDecoration = "";
         buttonMarkAsDone.textContent = "Mark as done";
-        task.status = "Not done";
+        singleTask.status = "Not done";
     }
 }
 
 function removeTasksFromPage() {
     const tableWithTasksList = document.getElementById('tasks-container');
     tableWithTasksList.innerHTML = "";
-    tasks = {};
+    tasksObject = {};
     console.log("Table is cleared", tableWithTasksList);
  }
 
